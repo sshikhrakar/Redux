438eff549dfbc8f18cfe1fc5b01924ffb138a382
Shirish Shikhrakar <shirish42@hotmail.com>
2016-07-25 13:21:31 +0545

    Connected to Redux

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>About</h1>
+        <p>React and Redux in ES6 about page.</p>
+      </div>
+    )
+  }
+}
+
+export default AboutPage;
